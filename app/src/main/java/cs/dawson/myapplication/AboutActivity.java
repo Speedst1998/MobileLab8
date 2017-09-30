917feed5e31e2e4001e2fbcfa57c112ef35aed18
Keylen <Keeson@hotmail.com>
2017-09-29 20:47:50 -0400

    the about activity properly fires

---
+++ app/src/main/java/cs/dawson/myapplication/AboutActivity.java
@@ -0,0 +1,17 @@
+package cs.dawson.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Keylen on 2017-09-29.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
